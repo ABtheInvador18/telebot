8dad80f6ac8c43bc09bb4b5fb8111ed3dec2450c
ABtheInvador18 <89350359+ABtheInvador18@users.noreply.github.com>
2022-01-04 17:30:29 +0530

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -5,9 +5,10 @@
 
 TOKEN = "5086123846:AAFnJQhbgPH38uW7SYZnoF2gZ9JCSMtz5Xw"
 import telegram
-bot = telegram.Bot(TOKEN)
-if bot.get_updates():
-    chat_id = bot.get_updates()[-1].message.chat_id
-    video = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
-    bot.send_video(chat_id, video)
+while (1 != 0):
+    bot = telegram.Bot(TOKEN)
+    if bot.get_updates():
+        chat_id = bot.get_updates()[-1].message.chat_id
+        video = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4"
+        bot.send_video(chat_id, video)
 
